b50fa251de1fc3c915c907938f1ffc6f5a7c5800
wffsffwt34terfsdgdhdrgwrvwwqew <38817400+Djlfsfghegfdfhfgw@users.noreply.github.com>
2018-06-30 21:07:37 +0300

    Update rape.js

--- cmds/rape.js
+++ cmds/rape.js
@@ -27,7 +27,7 @@ module.exports.run = async (bot, message, args) => {
 
             },
         {
-         name:"Template",
+         name:"Extra",
          value:(extra),
          inline:false
                 }
