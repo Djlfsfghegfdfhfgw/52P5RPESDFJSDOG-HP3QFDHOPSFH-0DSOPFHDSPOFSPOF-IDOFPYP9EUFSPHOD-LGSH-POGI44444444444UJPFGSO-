d3ceb9ced62a5eaa960dd1ddc3f7c48b04a7d4cf
wffsffwt34terfsdgdhdrgwrvwwqew <38817400+Djlfsfghegfdfhfgw@users.noreply.github.com>
2018-07-14 11:39:07 +0300

    Update 8ball.js

--- cmds/8ball.js
+++ cmds/8ball.js
@@ -4,7 +4,7 @@
 
     //!8ball question
     if(!args[1]) return message.reply("Plesae enter a full question with 2 or more words!");
-    let replies = ["Fangs doesn't like this question","Fangs says yes", "Nope","M8 please ask another question Fangs couldn't hear ya", "I don't know", "Ask again later!", "Oof", "I am not sure!", "Pls No", "You tell me", "Without a doubt", "Cannot predict now", "Without a doubt", ];
+    let replies = ["Fangs doesn't like this question","Fangs says yes", "Nope","M8 please ask another question Fangs couldn't hear ya", "I don't know", "Ask again later!I'm playing", "Oof", "I am not sure!", "Pls No", "You tell me", "Without a doubt", "Cannot predict now", "Without a doubt", ];
 
     let result = Math.floor((Math.random() * replies.length));
     let question = args.join(" ");
