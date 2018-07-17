cb9d29b8b86f9ca3188895b80bd032f09f3b8c45
wffsffwt34terfsdgdhdrgwrvwwqew <38817400+Djlfsfghegfdfhfgw@users.noreply.github.com>
2018-07-17 13:03:15 +0300

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -19,7 +19,7 @@ module.exports.run = async (bot, message, args) => {
 
     let funEmbed = new Discord.RichEmbed()
     .setColor("#0afffa")
-    .setTitle("**Fun commands (12)**")
+    .setTitle("**Fun commands (13)**")
     .setDescription(funcommands)
 
     message.channel.send(funEmbed);
