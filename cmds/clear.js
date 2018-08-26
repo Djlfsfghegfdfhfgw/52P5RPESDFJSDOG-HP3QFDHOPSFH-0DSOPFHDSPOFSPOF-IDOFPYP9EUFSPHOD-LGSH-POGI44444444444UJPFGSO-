c9387aabae5ff5653ba0cb5ba0578feb41265958
wffsffwt34terfsdgdhdrgwrvwwqew <38817400+Djlfsfghegfdfhfgw@users.noreply.github.com>
2018-08-26 13:17:35 +0300

    Update clear.js

--- cmds/clear.js
+++ cmds/clear.js
@@ -12,7 +12,7 @@ module.exports.run = async (bot, message, args) => {
 
   message.delete()
 
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You don't have premssions to do that!");
+  if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You don't have premssions to do that!");
   if(!args[0]) return message.channel.send("Please enter a number of messages to clear! `Usage: +catclear <amount>`");
   message.channel.bulkDelete(args[0]).then(() => {
   message.channel.send(`**__I deleted ${args[0]} messages.__**`).then(msg => msg.delete(2000));
