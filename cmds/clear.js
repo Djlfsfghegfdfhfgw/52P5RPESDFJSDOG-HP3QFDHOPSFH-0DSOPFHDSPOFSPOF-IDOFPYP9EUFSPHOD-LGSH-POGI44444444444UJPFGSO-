b8a57ab5d06963ad78035e58671cf8347797da6b
wffsffwt34terfsdgdhdrgwrvwwqew <38817400+Djlfsfghegfdfhfgw@users.noreply.github.com>
2018-08-26 13:22:00 +0300

    Update clear.js

--- cmds/clear.js
+++ cmds/clear.js
@@ -1,24 +1,12 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  if(args[0] == "help"){
-    let helpembxd = new Discord.RichEmbed()
-    .setColor("#0afffa")
-    .addField("clear Command", "Usage: +clear <amount>")
 
-    message.channel.send
-    return;
-  } 
-
-  message.delete()
-
-  if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You don't have premssions to do that!");
-  if(!args[0]) return message.channel.send("Please enter a number of messages to clear! `Usage: +catclear <amount>`");
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
+  if(!args[0]) return message.channel.send("oof");
   message.channel.bulkDelete(args[0]).then(() => {
-  message.channel.send(`**__I deleted ${args[0]} messages.__**`).then(msg => msg.delete(2000));
-});
-
-
+    message.channel.send(`I cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
+  });
 }
 
 module.exports.help = {
