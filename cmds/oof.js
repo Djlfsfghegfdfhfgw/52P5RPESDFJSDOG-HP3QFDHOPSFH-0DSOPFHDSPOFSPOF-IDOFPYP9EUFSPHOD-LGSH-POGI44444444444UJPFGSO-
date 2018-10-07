26c2d362f1b445d84e003fe2f13b4c41761e77d4
wffsffwt34terfsdgdhdrgwrvwwqew <38817400+Djlfsfghegfdfhfgw@users.noreply.github.com>
2018-10-07 12:06:06 +0300

    Add files via upload

---
+++ cmds/oof.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const fs = require("fs");
+module.exports.run = async (bot, message, args) => {
+  const rules = fs.readFileSync("./rules.txt", "utf8");
+  if(!message.member.hasPermission("ADMINISTRATOR")) return; 
+
+    message.channel.send({  
+     embed : {
+    title: "»»————- Welcome/Bun venit ————-««",
+    description:(rules),
+    color: 4886754
+}
+})
+
+message.delete();
+}
+module.exports.help = {
+name: "oof"
+}
