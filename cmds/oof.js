121cedafc8eee5e6391e1e00cf424c5f30734079
wffsffwt34terfsdgdhdrgwrvwwqew <38817400+Djlfsfghegfdfhfgw@users.noreply.github.com>
2018-10-07 13:02:00 +0300

    Update oof.js

--- cmds/oof.js
+++ cmds/oof.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
 
     message.channel.send({  
      embed : {
-    title: "»»————- Welcome/Bun venit/Welkom/Bienvenue/Bienvenido ————-««",
+    title: "»»————- Welcome/Bun venit/Welkom/Bienvenue ————-««",
     description:(rules),
     color: 4886754
 }
