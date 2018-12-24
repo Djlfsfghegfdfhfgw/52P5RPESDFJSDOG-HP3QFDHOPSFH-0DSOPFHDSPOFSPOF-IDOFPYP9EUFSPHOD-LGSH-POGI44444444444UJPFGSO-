b535a7b401a45c2843ff85be1ef087d942f2cb39
wffsffwt34terfsdgdhdrgwrvwwqew <38817400+Djlfsfghegfdfhfgw@users.noreply.github.com>
2018-12-24 07:24:41 +0200

    Update halp.js

--- cmds/halp.js
+++ cmds/halp.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
     message.channel.send({  
      embed : {
     title: "Informations",
-    description:"Howdy partner,welcome to the cult,gorgeous follower,I recommend you reading the <#381349368151932939> if you didn't,if you're not new then hi there :>,hope you have fun",
+    description:"Howdy partner,welcome to the cult,gorgeous follower,I recommend you reading the <#381349368151932939> if you didn't,if you're not new then howdy :>,hope you have fun",
     color: 4886754,
     fields: [
         {
@@ -16,17 +16,12 @@ module.exports.run = async (bot, message, args) => {
         },
         {
         name: "❃ ᴏ ᴛ ʜ ᴇ ʀ s ᴛ ᴜ ғ ғ s ❃",
-        value:"If you wonder where the other text channels are,just check the rules for some commands.",
+        value:"you can get your own colour and have access to other features if you're going to check <#505657455347367947>",
        inline:true
         },
         {
-        name:"✤ ᴄ ᴏ ᴍ ᴍ ᴀ ɴ ᴅ s ✤",
-        value:(commands),
-        inline:true
-        },
-        {
         name:"✩ s ᴛ ɪ ʟ ʟ ɴ ᴇ ᴇ ᴅ ʜ ᴇ ʟ ᴘ ? ✩",
-        value: "Well then,if you still need help you should ask someone,just ping the owner,the owner know e v e r y t h i n g",
+        value: "Well then,if you still need help you should ask someone,just ping the tsar,the tsar know e v e r y t h i n g",
         inline:true
         }
     ]
