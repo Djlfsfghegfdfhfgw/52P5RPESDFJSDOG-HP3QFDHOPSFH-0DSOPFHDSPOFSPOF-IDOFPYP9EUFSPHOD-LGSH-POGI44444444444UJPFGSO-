ebaf6b0ad84604691dafd01de8d52a4cbb80a83f
wffsffwt34terfsdgdhdrgwrvwwqew <38817400+Djlfsfghegfdfhfgw@users.noreply.github.com>
2018-12-27 06:50:05 +0200

    Update ping.js

--- cmds/ping.js
+++ cmds/ping.js
@@ -16,27 +16,6 @@ exports.run = async (client, message, args, color) => {
     });
 
 }
-
-exports.help = {
-    name: 'ping',
-    category: 'INFO'
-}const Discord = require('discord.js')
-
-exports.run = async (client, message, args, color) => {
-
-    let start = Date.now(); message.channel.send(message.channel.id, 'Pong! ').then(message => { 
-    let diff = (Date.now() - start); 
-    let API = (client.ping).toFixed(2)
-        
-        let embed = new Discord.RichEmbed()
-        .setTitle(`🔔 Pong!`)
-        .setColor(0xff2f2f)
-        .addField("📶 Latency", `${diff}ms`, true)
-        .addField("💻 API", `${API}ms`, true)
-        message.edit(embed);
-      
-    });
-}
  module.exports.help = {
     name: "ping"
   }
