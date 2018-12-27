fb74fd390397e704d0929f577d8e29f6fc8760aa
wffsffwt34terfsdgdhdrgwrvwwqew <38817400+Djlfsfghegfdfhfgw@users.noreply.github.com>
2018-12-27 06:47:58 +0200

    Update ping.js

--- cmds/ping.js
+++ cmds/ping.js
@@ -1,9 +1,42 @@
-module.exports.run = async (bot, message, args) => {
-message.channel.send('Pong! `' + bot.ping + '`ms')
+const Discord = require('discord.js')
 
-message.delete();
+exports.run = async (client, message, args, color) => {
+
+    let start = Date.now(); message.channel.send(message.channel.id, 'Pong! ').then(message => { 
+    let diff = (Date.now() - start); 
+    let API = (client.ping).toFixed(2)
+        
+        let embed = new Discord.RichEmbed()
+        .setTitle(`🔔 Pong!`)
+        .setColor(0xff2f2f)
+        .addField("📶 Latency", `${diff}ms`, true)
+        .addField("💻 API", `${API}ms`, true)
+        message.edit(embed);
+      
+    });
 
 }
-  module.exports.help = {
+
+exports.help = {
+    name: 'ping',
+    category: 'INFO'
+}const Discord = require('discord.js')
+
+exports.run = async (client, message, args, color) => {
+
+    let start = Date.now(); message.channel.send(message.channel.id, 'Pong! ').then(message => { 
+    let diff = (Date.now() - start); 
+    let API = (client.ping).toFixed(2)
+        
+        let embed = new Discord.RichEmbed()
+        .setTitle(`🔔 Pong!`)
+        .setColor(0xff2f2f)
+        .addField("📶 Latency", `${diff}ms`, true)
+        .addField("💻 API", `${API}ms`, true)
+        message.edit(embed);
+      
+    });
+}
+ module.exports.help = {
     name: "ping"
   }
