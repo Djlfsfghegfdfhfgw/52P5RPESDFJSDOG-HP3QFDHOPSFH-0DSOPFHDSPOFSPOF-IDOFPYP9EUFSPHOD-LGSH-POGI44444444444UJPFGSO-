18287aa3d85c48aab2280d9f2eae848f799fb272
wffsffwt34terfsdgdhdrgwrvwwqew <38817400+Djlfsfghegfdfhfgw@users.noreply.github.com>
2018-12-29 19:00:32 +0200

    Update timer.js

--- cmds/timer.js
+++ cmds/timer.js
@@ -4,18 +4,17 @@ const Discord = require("discord.js");
 
 module.exports.run = (bot, message, args, discord) => {
   let time = args.join(' ')
-  let embed = new discord.RichEmbed()
-  let descriptionEmbed = new Discord.RichEmbed()
+  let embed = new Discord.RichEmbed()
   .setTitle("Fangs Timer")
   .setDescription("Okay, I'll count down, and let you know when the time is up!")
   .setTimestamp()
   .setColor("RANDOM")
   .setFooter("Timer")
-  message.channel.send({embed: embed})
+  message.channel.send(embed);
   
   setTimeout(() => {
     embed.setDescription(`BEEP! The time's up! <@${message.author.id}>`)
-    message.channel.send({embed: embed})
+    message.channel.send(embed)
   }, ms(time))
 }
 
