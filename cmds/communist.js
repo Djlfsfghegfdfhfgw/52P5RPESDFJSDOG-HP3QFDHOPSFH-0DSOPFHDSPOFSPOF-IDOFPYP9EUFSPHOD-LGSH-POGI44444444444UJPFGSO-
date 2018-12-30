5394a5128ffb477390abed5fd868ca4e595b34d2
wffsffwt34terfsdgdhdrgwrvwwqew <38817400+Djlfsfghegfdfhfgw@users.noreply.github.com>
2018-12-30 10:10:46 +0200

    Update communist.js

--- cmds/communist.js
+++ cmds/communist.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args, prefix) => {
     let communist = Math.round(Math.random() * 100);
 
     let communistembed = new Discord.RichEmbed()
-        .setColor(random)
+        .setColor("#CD0000")
         .setTitle(`<:communistdoge:526621943340990464>  **I think ${message.author.username} is ${communist}% communist!** <:communistdoge:526621943340990464>`);
     message.delete(10);
     return message.channel.send(communistembed);
