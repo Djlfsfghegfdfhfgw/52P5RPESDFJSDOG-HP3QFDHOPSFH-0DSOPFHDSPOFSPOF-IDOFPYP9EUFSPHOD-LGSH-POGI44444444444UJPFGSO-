22344d82849e688a280e87e9894a0fa8d39c15fb
wffsffwt34terfsdgdhdrgwrvwwqew <38817400+Djlfsfghegfdfhfgw@users.noreply.github.com>
2019-01-10 11:40:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ fs.readdir("./cmds", (err, files) => {
 
 
 bot.on("guildMemberAdd", function(member) {
-    member.guild.channels.find("name", "welcome-room").send("<:1_:494901340426862592> "+ member.toString() +
+    member.guild.channels.find("name", "general").send("<:cookieman:395881188713168897> "+ member.toString() +
           " **Welcome™ to " + `${member.guild.name}` + "**\n" +
           "1⃣ Read the <#381349368151932939> if you're bored\n"+
           "2⃣ Read the recent <#381349547701436429> 📄\n"+
